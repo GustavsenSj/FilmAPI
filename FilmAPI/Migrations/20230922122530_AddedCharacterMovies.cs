5bb169ea57d3e7e2c10036e771fb303f787f2478
Philip Thangngat <philipcv@hotmail.com>
2023-09-22 14:40:42 +0200

    idk

---
+++ FilmAPI/Migrations/20230922122530_AddedCharacterMovies.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FilmAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCharacterMovies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CharacterMovie_Characters_CharactersId",
+                table: "CharacterMovie");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CharacterMovie_Movie_MoviesId",
+                table: "CharacterMovie");
+
+            migrationBuilder.RenameColumn(
+                name: "MoviesId",
+                table: "CharacterMovie",
+                newName: "MovieId");
+
+            migrationBuilder.RenameColumn(
+                name: "CharactersId",
+                table: "CharacterMovie",
+                newName: "CharacterId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CharacterMovie_MoviesId",
+                table: "CharacterMovie",
+                newName: "IX_CharacterMovie_MovieId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Gender",
+                table: "Characters",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "Characters",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Alias",
+                table: "Characters",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.InsertData(
+                table: "CharacterMovie",
+                columns: new[] { "CharacterId", "MovieId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 2, 4 }
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CharacterMovie_Characters_CharacterId",
+                table: "CharacterMovie",
+                column: "CharacterId",
+                principalTable: "Characters",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CharacterMovie_Movie_MovieId",
+                table: "CharacterMovie",
+                column: "MovieId",
+                principalTable: "Movie",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CharacterMovie_Characters_CharacterId",
+                table: "CharacterMovie");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CharacterMovie_Movie_MovieId",
+                table: "CharacterMovie");
+
+            migrationBuilder.DeleteData(
+                table: "CharacterMovie",
+                keyColumns: new[] { "CharacterId", "MovieId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "CharacterMovie",
+                keyColumns: new[] { "CharacterId", "MovieId" },
+                keyValues: new object[] { 2, 4 });
+
+            migrationBuilder.RenameColumn(
+                name: "MovieId",
+                table: "CharacterMovie",
+                newName: "MoviesId");
+
+            migrationBuilder.RenameColumn(
+                name: "CharacterId",
+                table: "CharacterMovie",
+                newName: "CharactersId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CharacterMovie_MovieId",
+                table: "CharacterMovie",
+                newName: "IX_CharacterMovie_MoviesId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Gender",
+                table: "Characters",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "Characters",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Alias",
+                table: "Characters",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60,
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CharacterMovie_Characters_CharactersId",
+                table: "CharacterMovie",
+                column: "CharactersId",
+                principalTable: "Characters",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CharacterMovie_Movie_MoviesId",
+                table: "CharacterMovie",
+                column: "MoviesId",
+                principalTable: "Movie",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
