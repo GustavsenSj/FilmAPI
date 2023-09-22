98b0c84acfe60c18de843ef197dc8bf40d4f5e1b
Sjur G <sjur.gustavsen@no.experis.com>
2023-09-22 14:06:35 +0200

    auto inc Id Franchise

--- FilmAPI/Data/Models/Franchise.cs
+++ FilmAPI/Data/Models/Franchise.cs
@@ -1,19 +1,21 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FilmAPI.Data.Models
 {
     public class Franchise
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        [StringLength(60)]
+        [StringLength(60)] 
         public string Name { get; set; } = null!;
-        public string? Description { get; set; } 
+        public string? Description { get; set; }
 
 
         /*------------------------iT's movies realtion stuff------------------------------*/
         // nav prop for franchise's related movies
         public ICollection<Movie> Movies { get; set; } = new List<Movie>();
         /*--------------------------------------------------------------------------------*/
-
     }
 }
