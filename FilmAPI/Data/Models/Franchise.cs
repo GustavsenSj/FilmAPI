a4fe767ed19ece40a4380c7b41892eb53f3c1720
Philip Thangngat <philipcv@hotmail.com>
2023-09-22 13:19:32 +0200

    Added new class for m2m relation between chr and movie within a class called MovieCharacters. Also established one2many realtion between franchise and movie (a movie's franchise can be null)

--- FilmAPI/Data/Models/Franchise.cs
+++ FilmAPI/Data/Models/Franchise.cs
@@ -1,6 +1,16 @@
-namespace FilmAPI.Data.Models;
-
-public class Franchise
+namespace FilmAPI.Data.Models
 {
-    
+    public class Franchise
+    {
+        public int FranchiseId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+
+        /*------------------------iT's movies realtion stuff------------------------------*/
+        // nav prop for franchise's related movies
+        public ICollection<Movie> Movies { get; set; } = new List<Movie>();
+        /*--------------------------------------------------------------------------------*/
+
+    }
 }
