eeaa04288272fb3f6685bfe1ada69a7406b63fa4
Philip Thangngat <philipcv@hotmail.com>
2023-09-22 12:32:32 +0200

    Character Class model finished.

--- FilmAPI/Data/Models/Character.cs
+++ FilmAPI/Data/Models/Character.cs
@@ -1,6 +1,15 @@
-namespace FilmAPI.Data.Models;
-
-public class Character
+namespace FilmAPI.Data.Models
 {
-    
+    public class Character
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Alias { get; set; }   
+        public string Gender { get; set; }
+        public string Picture { get; set; }
+        
+        public Movie Movie { get; set; } // Nav prop to realted movie.
+
+        public int MovieId { get; set; } // FK for related movies Id
+    }
 }
