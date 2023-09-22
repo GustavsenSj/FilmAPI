bdb6ce370ec034a8d09855e4249be041a10f29a6
Sjur G <sjur.gustavsen@no.experis.com>
2023-09-22 12:32:59 +0200

    Movie model

--- FilmAPI/Data/FilmDbContext.cs
+++ FilmAPI/Data/FilmDbContext.cs
@@ -1,8 +1,16 @@
+using FilmAPI.Data.Models;
 using Microsoft.EntityFrameworkCore;
-
 namespace FilmAPI.Data;
+
 
 public class FilmDbContext :DbContext
 {
+    public DbSet<Character> Characters { get; set; }
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<Franchise> Franchises { get; set; }
     
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("conection string here");
+    }
 }
--- FilmAPI/Data/Models/Movie.cs
+++ FilmAPI/Data/Models/Movie.cs
@@ -2,5 +2,11 @@ namespace FilmAPI.Data.Models;
 
 public class Movie
 {
-    
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Genre { get; set; }
+    public DateTime ReleaseYear { get; set; }
+    public string Director { get; set; }
+    public string? Picture { get; set; }
+    public string? Trailer { get; set; }
 }
