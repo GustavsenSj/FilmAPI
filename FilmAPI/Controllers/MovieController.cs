b49b94f9fab982355c316c08f4ab89c1f6657f8d
Sjur G <sjur.gustavsen@no.experis.com>
2023-09-25 13:05:57 +0200

    swagger documentation

--- FilmAPI/Controllers/MovieController.cs
+++ FilmAPI/Controllers/MovieController.cs
@@ -1,10 +1,15 @@
+using System.Net.Mime;
 using Microsoft.AspNetCore.Mvc;
 using FilmAPI.Data.Models;
 using FilmAPI.Services.Movie;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
 namespace FilmAPI.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
+[Produces(MediaTypeNames.Application.Json)]
+[ApiConventionType(typeof(DefaultApiConventions))]
 public class MovieController : ControllerBase
 {
     private readonly IMovieService _service;
@@ -14,10 +19,15 @@ public class MovieController : ControllerBase
         _service = service;
     }
 
+    /// <summary>
+    /// Get all movies in the database
+    /// </summary>
+    /// <returns> </returns>
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
     {
         return Ok(await _service.GetAllAsync());
+        //TODO: create exception to catch not found
     }
     
 }
